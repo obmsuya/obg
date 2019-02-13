2a58968110e1f5dc856fdb39898aec96984a582c
obmsuya <obmsuya@yahoo.com>
2019-02-13 14:47:25 +0300

    migration

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,36 +76,36 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 #live
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'obmsuya$obmsuya',
-        'USER': 'obmsuya',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            #Tell mysql to connect with 'utf8mb4' character set
-            'charset':'utf8mb4',
-            },
-        #Tell django to build the test database with 'utf8mb4'character set
-        'TEST': {
-            'CHARSET': 'utf8mb4',
-            'COLLATION': 'utf8mb4_unicode_ci',
-        },
-        'PASSWORD': 'Pa55w0rd',
-        'HOST': 'obmsuya.mysql.pythonanywhere-services.com',
-        'PORT': '',
-       
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME':'obmsuya$obmsuya',
+#        'USER': 'obmsuya',
+#        'OPTIONS': {
+#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#            #Tell mysql to connect with 'utf8mb4' character set
+#            'charset':'utf8mb4',
+#            },
+#        #Tell django to build the test database with 'utf8mb4'character set
+#        'TEST': {
+#            'CHARSET': 'utf8mb4',
+#            'COLLATION': 'utf8mb4_unicode_ci',
+#        },
+#        'PASSWORD': 'Pa55w0rd',
+#        'HOST': 'obmsuya.mysql.pythonanywhere-services.com',
+#        'PORT': '',
+#       
+#    }
+#}
 
 
 
