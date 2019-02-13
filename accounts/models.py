d648e055cfce74dd3918823c1b8ea82704fd5f11
obmsuya <obmsuya@yahoo.com>
2019-02-13 13:43:46 +0300

    database setting

--- accounts/models.py
+++ accounts/models.py
@@ -21,36 +21,7 @@ class UserProfile(models.Model):
     image2 = models.ImageField(upload_to='profile_image',null=True, blank=True)    
     debt = models.IntegerField(null=True, blank=True)
     
-    firstgenb = models.IntegerField(null=True, blank=True)
-    secondgenb = models.IntegerField(null=True, blank=True)
-    thirdgenb = models.IntegerField(null=True, blank=True)
-    fouthgenb = models.IntegerField(null=True, blank=True)
-    fifthgenb = models.IntegerField(null=True, blank=True)
     
-
-    firstgenr = models.IntegerField(null=True, blank=True)
-    secondgenr = models.IntegerField(null=True, blank=True)
-    fouthgenr = models.IntegerField(null=True, blank=True)
-    fifthgenr = models.IntegerField(null=True, blank=True)
-    
-    firstmthb = models.IntegerField(null=True, blank=True)
-    secondmthb = models.IntegerField(null=True, blank=True)
-    thirdmthb = models.IntegerField(null=True, blank=True)
-    fouthmthb = models.IntegerField(null=True, blank=True)
-    fifthmthb = models.IntegerField(null=True, blank=True)
-    sixthmthb = models.IntegerField(null=True, blank=True)
-    totalb = models.IntegerField(null=True, blank=True)
-
-    accountdescription = models.TextField (default='')
-    
-
-    firstmthr = models.IntegerField(null=True, blank=True)
-    secondmthr = models.IntegerField(null=True, blank=True)
-    thirdmthr = models.IntegerField(null=True, blank=True)
-    fouthmthr = models.IntegerField(null=True, blank=True)
-    fifthmthr = models.IntegerField(null=True, blank=True)
-    sixthmthr = models.IntegerField(null=True, blank=True)
-    totalr = models.IntegerField(null=True, blank=True)
     
     def __str__(self):
         return self.user.username
