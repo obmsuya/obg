9d2fde3bd50eba496cabcd6de7807d217c456d28
obmsuya <obmsuya@yahoo.com>
2019-02-13 12:59:47 +0300

    database setting

--- accounts/models.py
+++ accounts/models.py
@@ -5,30 +5,7 @@ from django.db.models.signals import post_save
 # Create your models here.
 
 
-DGENERATION_CHOICES = (
-    ('first','First'),
-    ('second', 'Second'),
-    ('third','Third'),
-    ('fouth','Fouth'),
-    ('fifth','Fifth'),
-    ('sixth','Sixth'),
-)
 
-MONTH_CHOICES = (
-    ('january','January'),
-    ('february', 'February'),
-    ('march','March'),
-    ('april','April'),
-    ('may','May'),
-    ('june','June'),
-    ('july','July'),
-    ('august','August'),
-    ('september','September'),
-    ('october','October'),
-    ('november','November'),
-    ('december','December'),
-    
-)
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     fullname = models.CharField (max_length=50, default='')
@@ -38,7 +15,7 @@ class UserProfile(models.Model):
     Proffession = models.CharField (max_length=100, default='')
     country = models.CharField (max_length=100, default='')
     region = models.CharField (max_length=100, default='')
-    
+    city = models.CharField (max_length=100, default='')
     phone = models.IntegerField(default=0)
     image = models.ImageField(upload_to='profile_image',null=True, blank=True)
     image2 = models.ImageField(upload_to='profile_image',null=True, blank=True)    
@@ -49,20 +26,25 @@ class UserProfile(models.Model):
     thirdgenb = models.IntegerField(default=0)
     fouthgenb = models.IntegerField(default=0)
     fifthgenb = models.IntegerField(default=0)
+    
+
+    firstgenr = models.IntegerField(default=0)
+    secondgenr = models.IntegerField(default=0)
+    thirdgenr = models.IntegerField(default=0)
+    fouthgenr = models.IntegerField(default=0)
+    fifthgenr = models.IntegerField(default=0)
+    
     firstmthb = models.IntegerField(default=0)
-    city = models.CharField (max_length=100, default='')
     secondmthb = models.IntegerField(default=0)
     thirdmthb = models.IntegerField(default=0)
     fouthmthb = models.IntegerField(default=0)
     fifthmthb = models.IntegerField(default=0)
     sixthmthb = models.IntegerField(default=0)
     totalb = models.IntegerField(default=0)
+
     accountdescription = models.TextField (default='')
-    firstgenr = models.IntegerField(default=0)
-    secondgenr = models.IntegerField(default=0)
-    thirdgenr = models.IntegerField(default=0)
-    fouthgenr = models.IntegerField(default=0)
-    fifthgenr = models.IntegerField(default=0)
+    
+
     firstmthr = models.IntegerField(default=0)
     secondmthr = models.IntegerField(default=0)
     thirdmthr = models.IntegerField(default=0)
