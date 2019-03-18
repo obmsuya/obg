629788974bd0b7629b209a5472cbb8d57dd6ac99
Overblessing <obmsuya@yahoo.com>
2019-03-18 11:00:15 +0300

    settings

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,20 +2,20 @@ from django.contrib import admin
 from .models import UserProfile
 
 # Register your models here.
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ( 'country','phone','user_info')
+# class UserProfileAdmin(admin.ModelAdmin):
+#     list_display = ( 'country','phone','user_info')
 
-    def user_info(self,obj):
-        return obj.Proffession
+#     def user_info(self,obj):
+#         return obj.Proffession
 
 
-    def get_queryset(self, request):
-        queryset= super(UserProfileAdmin,self).get_queryset(request)
-        queryset= queryset.order_by('phone')
-        return queryset
+#     def get_queryset(self, request):
+#         queryset= super(UserProfileAdmin,self).get_queryset(request)
+#         queryset= queryset.order_by('phone')
+#         return queryset
 
-    user_info.short_description = 'Info'
+#     user_info.short_description = 'Info'
 
-admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserProfile)
 
 
--- accounts/models.py
+++ accounts/models.py
@@ -7,40 +7,47 @@ from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
-    # user = models.OneToOneField(User,unique=True, on_delete=models.CASCADE)
-    fullname = models.CharField (max_length=50, default='')
+    
+    user = models.OneToOneField(User,unique=True, on_delete=models.CASCADE, null=True)
     upliner = models.CharField (max_length=50, default='')
-    uplinerphone = models.IntegerField(default=0)
-    Maelezo = models.TextField()
-    profilemake = models.CharField (max_length=100, default='')
-    backofice = models.CharField (max_length=100, default='')
+    downliner = models.IntegerField(default=0)
+    Maelezo = models.CharField (max_length=100, default='')
+    Proffession = models.CharField (max_length=100, default='')
     country = models.CharField (max_length=100, default='')
     region = models.CharField (max_length=100, default='')
-    
+    city = models.CharField (max_length=100, default='')
+    #website = models.URLField(default='')
     phone = models.IntegerField(default=0)
     image = models.ImageField(upload_to='profile_image',null=True, blank=True)
-    image2 = models.ImageField(upload_to='profile_image',null=True, blank=True)    
+    image2 = models.ImageField(upload_to='profile_image',null=True, blank=True)
+    account = models.IntegerField(default=0)
     debt = models.IntegerField(default=0)
+    accountdescription = models.CharField (max_length=200, default='')
     
     firstgenb = models.IntegerField(default=0)
     secondgenb = models.IntegerField(default=0)
     thirdgenb = models.IntegerField(default=0)
     fouthgenb = models.IntegerField(default=0)
     fifthgenb = models.IntegerField(default=0)
+    
+
+    firstgenr = models.IntegerField(default=0)
+    secondgenr = models.IntegerField(default=0)
+    thirdgenr = models.IntegerField(default=0)
+    fouthgenr = models.IntegerField(default=0)
+    fifthgenr = models.IntegerField(default=0)
+    
     firstmthb = models.IntegerField(default=0)
-    city = models.CharField (max_length=100, default='')
     secondmthb = models.IntegerField(default=0)
     thirdmthb = models.IntegerField(default=0)
     fouthmthb = models.IntegerField(default=0)
     fifthmthb = models.IntegerField(default=0)
     sixthmthb = models.IntegerField(default=0)
     totalb = models.IntegerField(default=0)
-    accountdescription = models.TextField (default='')
-    firstgenr = models.IntegerField(default=0)
-    secondgenr = models.IntegerField(default=0)
-    thirdgenr = models.IntegerField(default=0)
-    fouthgenr = models.IntegerField(default=0)
-    fifthgenr = models.IntegerField(default=0)
+
+   
+    
+
     firstmthr = models.IntegerField(default=0)
     secondmthr = models.IntegerField(default=0)
     thirdmthr = models.IntegerField(default=0)
@@ -48,9 +55,9 @@ class UserProfile(models.Model):
     fifthmthr = models.IntegerField(default=0)
     sixthmthr = models.IntegerField(default=0)
     totalr = models.IntegerField(default=0)
-    
+
     def __str__(self):
-        return self.fullname
+        return self.user.username
     
     
     
@@ -59,11 +66,11 @@ class UserProfile(models.Model):
     
     
 
-# def create_profile(sender, **kwargs):
-#     if kwargs['created']:
-#             user_profile = UserProfile.objects.create(user=kwargs['instance'])
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+            user_profile = UserProfile.objects.create(user=kwargs['instance'])
 
-# post_save.connect(create_profile, sender=User)
+post_save.connect(create_profile, sender=User)
 
 
 
